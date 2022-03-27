db135a00bc5e9ea175ecb0ec8441e8d0997c7208
IFEELS3UPID <akarit2001@gmail.com>
2022-03-28 01:05:30 +0700

    edit y 01:05AM

--- program/customer.php
+++ program/customer.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 ?>
-<?php
+<? 
     // Connect
     $servername = "127.0.0.1";
     $username = "root";
@@ -11,7 +11,7 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     $conn->set_charset("utf8");
-    // Check connection
+    // Check connections
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
